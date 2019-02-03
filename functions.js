ab3d2e23bba058a21dcbdb8c73421f430b3a20ba
Alice <ipa1@mcmaster.ca>
2019-02-02 23:36:47 -0500

    Jquery function edit

--- functions.js
+++ functions.js
@@ -2,11 +2,14 @@
 function showSentiment1(){
 	var isentence = $("#sentence").val(); // input of textbox in isentence
 	$("#sentiment").html(isentence);
-
 }
 
 function showSentiment2(str) {
 	var lng = str.length;
+	var isentence = $("#sentence").val();
+	$("#sentiment").html(isentence);
+	$("#popup").css("opacity","0.8")
+
 	document.getElementById("sentiment2").innerHTML = lng + ' out of 400 characters';
 }
 
